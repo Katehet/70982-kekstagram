0a1f000b02da6dedd4ec8a2f810e2f32f3f05e91
Katehet <sv.sleep@gmail.com>
2017-12-22 23:48:47 +0300

    Реализует открытие и закрытие окна редактирования фото

--- js/pictures.js
+++ js/pictures.js
@@ -20,6 +20,24 @@ var maxPicturesNumber = 25;
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 
+// Переменные для работы с формами
+var uploadForm = document.querySelector('#upload-select-image');
+var fileInput = uploadForm.querySelector('#upload-file');
+var editForm = document.querySelector('.upload-overlay');
+var closeButton = editForm.querySelector('.upload-form-cancel');
+
+var field = editForm.querySelector('.upload-form-description');
+
+var decreaseButton = editForm.querySelector('.upload-resize-controls-button-dec');
+var increaseButton = editForm.querySelector('.upload-resize-controls-button-inc');
+var resizeValue = editForm.querySelector('.upload-resize-controls-value').value;
+
+var value = parseInt(resizeValue, 10);
+var minPicSize = parseInt(editForm.querySelector('.upload-resize-controls-value').min, 10);
+var maxPicSize = parseInt(editForm.querySelector('.upload-resize-controls-value').max, 10);
+
+// var currentPhoto = editForm.querySelector('.effect-image-preview');
+
 // Возвращает сучайное число в диапазоне [min, max) (для лайков)
 var getRandomValue = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -95,48 +113,74 @@ pictures.appendChild(fragment);
 
 // Функции открытия/закрытия картинки
 var escapePress = function (event) {
-  if (event.keyCode === ESC_KEYCODE) {
+  if (event.keyCode === ESC_KEYCODE) { // Закрытие по нажатию Esc
     closeGallery();
   }
 };
 
 var openGallery = function () {
   galleryOverlay.classList.remove('hidden');
-  document.addEventListener('keydown', escapePress);
+  document.addEventListener('keydown', escapePress); // Открытие галереи, прослушка на Esc
 };
 
 var closeGallery = function () {
   galleryOverlay.classList.add('hidden');
-  document.removeEventListener('keydown', escapePress);
+  document.removeEventListener('keydown', escapePress);// Закрытие галереи, удаление прослушки Esc
 };
 
 // Обработчики событий клика/нажатия клавиш
-pictures.addEventListener('click', function (event) {
+pictures.addEventListener('click', function (event) { // открывает галерею по клику на IMG
   event.preventDefault();
   if (event.target.tagName === 'IMG') {
     openGallery();
     renderOverlayPicture(event);
   }
-  // pic.addEventListener('click', renderOverlayPicture);
 });
 
-pictures.addEventListener('keydown', function (event) {
+pictures.addEventListener('keydown', function (event) { // Открывает галерею по нажатию Enter при фокусе на picture
   event.preventDefault();
   if (event.keyCode === ENTER_KEYCODE) {
     openGallery();
   }
 });
 
-closePicture.addEventListener('keydown', function (event) {
+closePicture.addEventListener('keydown', function (event) { // Закрывает галерею по нажатию Enter при фокусе на "крестике"
   event.preventDefault();
   if (event.keyCode === ENTER_KEYCODE) {
     closeGallery();
   }
 });
 
-closePicture.addEventListener('click', function () {
+closePicture.addEventListener('click', function () { // Закрывает галерею по клику на "крестик"
   event.preventDefault();
   closeGallery();
+});
+
+// Показ/скрытие формы редактирования фото
+var onFormEscapePress = function (event) {
+  if (event.keyCode === ESC_KEYCODE && !(field === document.activeElement)) {
+    closeEditForm();
+  }
+};
+var openEditForm = function () {
+  editForm.classList.remove('hidden');
+  document.addEventListener('keydown', onFormEscapePress);
+};
+var closeEditForm = function () {
+  editForm.classList.add('hidden');
+  document.removeEventListener('keydown', onFormEscapePress);
+};
+
+// Обработчики событий форм
+fileInput.addEventListener('change', openEditForm); // "Слушает" input[type='file']
+
+closeButton.addEventListener('click', closeEditForm); // "Слушает" клик на "крестик" формы
+
+closeButton.addEventListener('keydown', function () { // "Слушает" нажатие Enter при фокусе на "крестике"
+  event.preventDefault();
+  if (event.keyCode === ENTER_KEYCODE) {
+    closeGallery();
+  }
 });
 
 // Отрисовывает полноэкранное фото с количеством комментариев и лайков
@@ -150,4 +194,27 @@ var renderOverlayPicture = function (event) {
   galleryOverlay.querySelector('.comments-count').textContent = clickedPhotoComments;
 
 };
+
+// Уменьшение фото
+decreaseButton.addEventListener('click', function () {
+  if (value > minPicSize) {
+    value = value - 25;
+  }
+  resizeValue = value + '%';
+  editForm.querySelector('.upload-resize-controls-value').value = resizeValue;
+});
+
+// Увеличение фото
+increaseButton.addEventListener('click', function () {
+  if (value < maxPicSize) {
+    value = value + 25;
+  }
+  resizeValue = value + '%';
+  editForm.querySelector('.upload-resize-controls-value').value = resizeValue;
+});
+
+
+// Применение эффектов к фото
+
+
 renderOverlayPicture();
